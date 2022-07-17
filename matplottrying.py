3da73073c6a034caad27ee560c744c7d39e26ce0
strandjd <107058931+strandjd@users.noreply.github.com>
2022-07-17 22:08:34 +0200

    Add files via upload
    matplotlab with scatter-plot. index on y-axis and temperature on x.

--- matplottrying.py
+++ matplottrying.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 import os
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# reading the database
-
 data = pd.read_json(CURR_DIR_PATH + "/target_data/" + "data.json")
-print(data)
+
+print(data.index)
+# # Scatter plot with day against tip
+#plt.scatter(data['air pressure'], data['temperature'])
+plt.scatter(data.index, data['temperature'])
 
 
-# Line plot with day against tip
-plt.plot(data['temperature'])
+
 #plt.plot(data['precipitation'])
 
 # Adding Title to the Plot
-plt.title("Line Plot")
+plt.title("Scatter Plot")
 
 # Setting the X and Y labels
 plt.xlabel('Hour')
