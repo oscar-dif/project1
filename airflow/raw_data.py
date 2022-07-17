336ea7d679e1f921c666034675893a9a3e6bd932
strandjd <107058931+strandjd@users.noreply.github.com>
2022-07-17 23:21:36 +0200

    Add files via upload
    #vi behöver bygga ihop parameterchoice och create_harmony_dict för koden till dag måste gå i en linje. den får inte loopa

--- airflow/raw_data.py
+++ airflow/raw_data.py
@@ -1,54 +1,126 @@
-import requests, os, configparser
-import pandas
-import matplotlib.pyplot
-import json
-from sqlalchemy import create_engine
-import psycopg2 as ps
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
+
 from datetime import datetime, timedelta
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-			READ THIS!
-IF YOU WANT TO TEST THIS FILE YOU NEED TO HAVE A FOLDER,
-			"raw_data", 
-	     IN THE SAME PLACE AS THIS FILE.
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-
-
-
-
+import requests, os, configparser
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+from sqlalchemy import *
+import psycopg2 as ps
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 data_dir = CURR_DIR_PATH + "/raw_data/" + "data.json"
-# target_dir = CURR_DIR_PATH + "/target_data/" + "data.json"
+target_dir = CURR_DIR_PATH + "/target_data/" + "data.json"
+
+# Initializes configuration from the config.ini file
+config = configparser.ConfigParser()
+config.read(CURR_DIR_PATH + "/config.ini")
+
+# Fetches the api key from your config.ini file
+API_KEY = config.get("DEV", "API_KEY")
+API_USER = config.get("DEV", "USER")
 
 
-
+#conn_string = f"postgresql://{API_USER}:{API_KEY}@localhost:5432/weather_data"
+conn_string = f"postgresql://{API_USER}:{API_KEY}@127.0.0.1/weather_data"
+#conn_string = f"postgresql://{API_USER}:{API_KEY}@localhost/weather_data"
+db = create_engine(conn_string)
+conn = db.connect()
 WEATHER_URL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16/lat/58/data.json"
-smhi = requests.get(WEATHER_URL)
 
+#******************************************************************************************
+def request():
+    r = requests.get(WEATHER_URL)
+    return r
 
-
-def raw_to_harmonized():
-    smhi = requests.get(WEATHER_URL)
-
-    if smhi.status_code == 200: # If connection is successful (200: http ok)
-        json_data = smhi.json() # Get result in json
-    
+def rawdata(): #gets api data and saves in json file
+    r = request()
+    if r.status_code == 200: # If connection is successful (200: http ok)
+        json_data = r.json() # Get result in json
     with open(data_dir, "w") as f:
-        return json.dump(json_data, f)
+        json.dump(json_data, f)
 
 
-#### 2
+
+#vi behöver bygga ihop parameterchoice och create_harmony_dict för koden till dag måste gå i en linje. den får inte loopa
+
+def parameterchoice(choice): #searches for data in api
+    r = request()
+    dict = json.loads(r.text)
+    result = {} 
+    for n in range(len(dict['timeSeries'])):
+        for i in range(len(dict['timeSeries'][n]["parameters"])):            
+            if dict['timeSeries'][n]["parameters"][i]['name'] == choice:
+                choice_value = dict['timeSeries'][n]["parameters"][i]['values'][0]  
+        result[f"{n}"] = choice_value
+    return result
+
+
+
+
+
+
+def create_harmony_dict(): #creates dict from selected data
+    df = pd.DataFrame(weather_data) 
+    weather_data = {
+                    "temperature": parameterchoice('t'),
+                    "air pressure": parameterchoice('pmean'),
+                    "precipitation": parameterchoice('msl')
+                }
+    df.to_json(target_dir) 
+
+
+# def save_harmony_data(): #saves harmonized data in json
+#     df = pd.DataFrame(create_harmony_dict()) 
+#     df.to_json(target_dir) 
+
+
+
+
+
+# def sql_transfer():
+#     df = pd.DataFrame(create_harmony_dict()) 
+#     df.to_sql('weather_data', conn, if_exists='replace', index=False)
+#     conn.autocommit = True 
+#     weather1 = db.execute(text("select * from weather_data;"))
+#     #for i in weather1:
+#       #  print(i)
+
+
+
+
+#####*****************************DAG*********************************************
 with DAG("raw_data", start_date=datetime(2021, 1, 1),
-    schedule_interval=timedelta(minutes=1), catchup=False) as dag: 
+    schedule_interval=timedelta(minutes=1), catchup=False) as dag:
 
-        raw_to_harmonized = PythonOperator(
-            task_id="raw_to_harmonized",
-            python_callable=raw_to_harmonized
+        rawdata = PythonOperator(
+            task_id="rawdata",
+            python_callable=rawdata
         )
 
-        [raw_to_harmonized]
+        parameterchoice = PythonOperator(
+            task_id = "choice_of_data",
+            python_callable=parameterchoice
+        )
+        
+        create_harmony_dict = PythonOperator(
+            task_id = "harmonized",
+            python_callable=create_harmony_dict
+        )
+        
+        # save_harmony_data = PythonOperator(
+        #     task_id = "save_harmony_data",
+        #     python_callable=save_harmony_data
+        # )
+
+        # sql_transfer = PythonOperator(
+        #     task_id = "data_to_postgres_db",
+        #     python_callable=sql_transfer
+        # )
+
+
+
+        #[training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
+        #[rawdata] >> [parameterchoice] >> [create_harmony_dict] ##>> [save_harmony_data] >> [sql_transfer] 
