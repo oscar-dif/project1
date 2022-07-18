11abfabf1645a19b85b2fd2128045ef852d2ba64
strandjd <107058931+strandjd@users.noreply.github.com>
2022-07-18 10:42:55 +0200

    Add files via upload
    work on dags task

--- weather_dag.py
+++ weather_dag.py
@@ -2,9 +2,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 
-
-from datetime import datetime
-import requests, os
+from datetime import datetime, timedeltas
+import requests, os, configparser
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
@@ -15,7 +14,17 @@ CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 data_dir = CURR_DIR_PATH + "/raw_data/" + "data.json"
 target_dir = CURR_DIR_PATH + "/target_data/" + "data.json"
 
-conn_string = "postgresql://postgres:pass@127.0.0.1/weather_data"
+# Initializes configuration from the config.ini file
+config = configparser.ConfigParser()
+config.read(CURR_DIR_PATH + "/config.ini")
+
+# Fetches the api key from your config.ini file
+API_KEY = config.get("DEV", "API_KEY")
+USER = config.get("DEV", "USER")
+
+
+conn_string = f"postgresql://{USER}:{API_KEY}@127.0.0.1/weather_data"
+#conn_string = f"postgresql://postgres:{API_KEY}@localhost/weather_data"
 db = create_engine(conn_string)
 conn = db.connect()
 WEATHER_URL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16/lat/58/data.json"
@@ -29,9 +38,12 @@ def rawdata(): #gets api data and saves in json file
     r = request()
     if r.status_code == 200: # If connection is successful (200: http ok)
         json_data = r.json() # Get result in json
-    # 6     Denna har "w" som ger att skriva över. Ska vi skriva koden så den tittar om ett dokument med det namnet redan finns och gör nånting sen? 
     with open(data_dir, "w") as f:
         json.dump(json_data, f)
+
+
+
+
 
 def parameterchoice(choice): #searches for data in api
     r = request()
@@ -44,44 +56,26 @@ def parameterchoice(choice): #searches for data in api
         result[f"{n}"] = choice_value
     return result
 
-def date_collect():
-    r = request()
-    dict = json.loads(r.text)
-    result = {}
-    for n in range(len(dict['timeSeries'])):
-        datedata = dict['timeSeries'][n]["validTime"]
-        datedata = datedata[2:10] + " " + datedata[11:16]  #comment out to keep the original formatting
-        result[f"{n}"] = datedata   
-    return result
-    
+
+
+
+
+
 def create_harmony_dict(): #creates dict from selected data
     weather_data = {
-                    "date": date_collect(),
                     "temperature": parameterchoice('t'),
                     "air pressure": parameterchoice('pmean'),
                     "precipitation": parameterchoice('msl')
                 }
     return weather_data
 
-def line_plot():
-    data = pd.read_json(CURR_DIR_PATH + "/target_data/" + "data.json")
-    fig, ax = plt.subplots() 
-    plt.title("Line Plot")
-    # Line plot with day against tip
-    ax.plot(data['temperature'], color = 'green')
-    ax.tick_params(axis='y', labelcolor='green')
-    ax.set_xlabel('Hourly')
-    ax.set_ylabel('Temperature')    
-    #adding second y axis
-    ax2 = ax.twinx()
-    ax2.set_ylabel('Precipitation')
-    ax2.plot(data['precipitation'], color = 'blue')
-    ax2.tick_params(axis='y', labelcolor='blue')
-    plt.show()
-
 def save_harmony_data(): #saves harmonized data in json
     df = pd.DataFrame(create_harmony_dict()) 
     df.to_json(target_dir) 
+
+
+
+
 
 def sql_transfer():
     df = pd.DataFrame(create_harmony_dict()) 
@@ -93,35 +87,41 @@ def sql_transfer():
 
 
 
-#####*****************************DAG*********************************************
-with DAG("weather_dag", start_date=datetime(2022, 7, 14),
-    schedule_interval="@hourly", catchup=False) as dag:
 
-        save_rawdata = PythonOperator(
-            task_id = "save_rawdata",
+#####*****************************DAG*********************************************
+with DAG("raw_data", start_date=datetime(2021, 1, 1),
+    schedule_interval=timedelta(minutes=1), catchup=False) as dag:
+
+        rawdata = PythonOperator(
+            task_id="rawdata",
             python_callable=rawdata
         )
 
+
+
+#-----------------------------------------------------------------------------#
+
+        parameterchoice = PythonOperator(
+            task_id = "choice_of_data",
+            python_callable=parameterchoice('t')
+        )
+        
         create_harmony_dict = PythonOperator(
-            task_id = "create_harmony_dict",
+            task_id = "harmonized",
             python_callable=create_harmony_dict
         )
         
-
-        line_plot = PythonOperator(
-            task_id = "line_plot",
-            python_callable=line_plot
-        )
-
-        harmony_dict = PythonOperator(
-            task_id = "harmony_dict",
+        save_harmony_data = PythonOperator(
+            task_id = "save_harmony_data",
             python_callable=save_harmony_data
         )
 
-        to_sql = PythonOperator(
-            task_id = "save_to_sql",
+        sql_transfer = PythonOperator(
+            task_id = "data_to_postgres_db",
             python_callable=sql_transfer
         )
 
 
-        [save_rawdata, create_harmony_dict] >> line_plot >> harmony_dict >> to_sql
+
+        #[training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
+        [rawdata] >> [parameterchoice] >> [create_harmony_dict] >> [save_harmony_data] >> [sql_transfer] 
